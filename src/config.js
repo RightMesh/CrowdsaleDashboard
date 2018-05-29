40bc0809da6cf577f2f13f559332c943f9f03ace
Igor <igor@rightmesh.io>
2018-05-29 13:30:09 -0700

    updated config file

--- src/config.js
+++ src/config.js
@@ -1,9 +1,8 @@
 module.exports = {
-    crowdsaleAddress:   '0xEA1Fd6e4fd58bc4D4a1bcd91e7877662B002ff6b',
-    //crowdsaleAddress:   '0x00D51DE2a114d5CB1b68feC719F4F92321Cfe6eD',
+    crowdsaleAddress:   '0x00D51DE2a114d5CB1b68feC719F4F92321Cfe6eD',
     httpProvider:       'http://localhost:8545',
     noMetamask:         false,
     advanced:           false,
-    testTransactions:   true,
+    testTransactions:   false,
     pollInterval:       5000
 }
